8f7943e9ff762e5a1a3eb191fb6dcd2df1439abf
David Almaraz <david.almaraz@a2odev.com>
2022-06-22 09:01:03 -0400

    Delete unnecesary features

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
@@ -15,13 +13,6 @@ use App\Http\Controllers\ProductController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::middleware('client')->group(function () {
     Route::apiResources([
