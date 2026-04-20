be91cc852c014a186e8da92738b49d788abd9322
Shirley Quelali <shirley.quelali@a2odev.com>
2026-04-20 14:55:17 -0400

    feature/RI-468: Add stripe client

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Cashier\Cashier;
+use Stripe\StripeClient;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->app->singleton(StripeClient::class, fn() => Cashier::stripe());
     }
 
     public function boot()
