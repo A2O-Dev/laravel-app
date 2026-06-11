9e699278fd53abbcf5be5cf96a955d65a31ca6ee
Shirley Quelali <shirley.quelali@a2odev.com>
2026-06-11 12:07:24 -0400

    feature/RI-2395: Fix component re-exports to use named default exports

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,5 +1,5 @@
-export * from './AuthCard';
-export * from './EmailField';
-export * from './FormAlert';
-export * from './PasswordConfirmFields';
-export * from './PasswordField';
+export { default as AuthCard } from './AuthCard';
+export { default as EmailField } from './EmailField';
+export { default as FormAlert } from './FormAlert';
+export { default as PasswordConfirmFields } from './PasswordConfirmFields';
+export { default as PasswordField } from './PasswordField';
